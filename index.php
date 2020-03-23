4a151ee5777df0a60edb2155e5996d840278ce4d
Romain Tello <romain.tello@gmail.com>
2020-03-23 15:39:11 +0100

    Add form with POST method

--- index.php
+++ index.php
@@ -32,20 +32,31 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>
-        Bonjour <?php echo $_GET['name'] ?>
-    </h1>
+    <form method="post">
+        <label for="name">Votre nom</label>
+        <input type="text" name="name" />
+        <label for="age">Votre âge</label>
+        <input type="number" name="age" />
+        <input type="submit" />
+    </form>
 
-    <h2>Liste des films:</h2>
-    <ul>
-        <?php foreach($films as $film): ?>
-            <?php if ($film['age'] <= $_GET['age']): ?>
-                <li>
-                    <?php echo $film['title'] ?>
-                </li>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </ul>
+
+    <?php if (isset($_POST['name']) && isset($_POST['age'])): ?>
+        <h1>
+            Bonjour <?php echo $_POST['name'] ?>
+        </h1>
+
+        <h2>Liste des films:</h2>
+        <ul>
+            <?php foreach($films as $film): ?>
+                <?php if ($film['age'] <= $_POST['age']): ?>
+                    <li>
+                        <?php echo $film['title'] ?>
+                    </li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 
 </body>
 </html>
